0232790e9dc1fcca37ac2033f44bc4b10f3fabd1
AbdallahShnaino <abdallah.shnaino@gmail.com>
2022-08-29 10:18:34 +0300

    init

--- db/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-// create user model and export it 
-const User = mongoose.model('User', {
-     username: String,
-     firstName: String,
-     lastName: String
-     });
-module.exports = User
